ac553c73a9105ff0dc993719e27eaf73594ff1b8
FL42 <46161216+fl42@users.noreply.github.com>
2019-02-13 10:39:43 +0100

    Start all config files in config dir, use sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,25 +1,24 @@
-#!/bin/bash
+#!/bin/sh
 # Author: FL42
 
 set -e
 
-# This script expects a directory /config with one file CONFIG which defines
-# one path to a config file to run per line (e.g. /config/example.yaml)
-# /config should be mounted by Docker at container startup.
-
-CONFIG_FILE="/config/CONFIG"
-
-if [ ! -e $CONFIG_FILE ]
-then
-    echo "$CONFIG_FILE does not exist"
+# Check for config files
+if [ -z "$(ls -A /config)" ]; then
+    echo "ERROR: config directory is empty, exiting!"
     exit 1
 fi
 
-cat $CONFIG_FILE | while read config_path
+# Loop on config files
+for config_file in /config/*.yaml
 do
+    echo "Running with config file $config_file..."
     # -u disables buffering for stdout
-    python -u monitoring.py -c $config_path &
+    python -u monitoring.py -c $config_file &
 done
+
+# Forward SIGTERM to python
+trap 'pkill --signal SIGTERM python' SIGTERM
 
 # Don't exit
 while true
