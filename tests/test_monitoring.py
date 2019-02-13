09561510ad48359f8c44f3fd3581e1b025e3f590
FL42 <46161216+fl42@users.noreply.github.com>
2019-02-13 09:43:33 +0100

    Add tests for monitoring logic

--- tests/test_monitoring.py
+++ tests/test_monitoring.py
@@ -51,6 +51,23 @@ class TestNotificationManagementLogic(unittest.TestCase):
             self.services_monitoring.down_services == [message1]
         )
 
+    def test_few_notifications(self):
+        """
+        Test with few notifications
+        """
+        message1 = Message('Service 1', 'Message 1', Message.ERROR)
+        message2 = Message('Service 2', 'Message 2', Message.ERROR)
+        notifications = [message1, message2]
+        self.services_monitoring.down_services = []
+
+        notifications_to_send = \
+            self.services_monitoring.manage_notifications(notifications)
+
+        self.assertTrue(notifications_to_send == [message1, message2])
+        self.assertTrue(
+            self.services_monitoring.down_services == [message1, message2]
+        )
+
     def test_back_online(self):
         """
         Test if a service was down but it's now up
@@ -73,6 +90,38 @@ class TestNotificationManagementLogic(unittest.TestCase):
         self.assertTrue(notifications_to_send == expected_notifications)
         self.assertTrue(not self.services_monitoring.down_services)
 
+    def test_back_online_multiple_services(self):
+        """
+        Test if services were down but are now up
+        """
+        message1 = Message('Service 1', 'Message 1', Message.ERROR)
+        message2 = Message('Service 2', 'Message 2', Message.ERROR)
+        message3 = Message('Service 3', 'Message 3', Message.WARNING)
+        expected_notifications = [
+            Message(
+                'Service 1',
+                'Message 1',
+                Message.ERROR,
+                'back online'
+            ),
+            Message(
+                'Service 2',
+                'Message 2',
+                Message.ERROR,
+                'back online'
+            )
+        ]
+        notifications = [message3]
+        self.services_monitoring.down_services = [message1, message2, message3]
+
+        notifications_to_send = \
+            self.services_monitoring.manage_notifications(notifications)
+
+        self.assertTrue(notifications_to_send == expected_notifications)
+        self.assertTrue(
+            self.services_monitoring.down_services == [message3]
+        )
+
     def test_still_down(self):
         """
         Test if a service is still down
@@ -87,4 +136,21 @@ class TestNotificationManagementLogic(unittest.TestCase):
         self.assertTrue(not notifications_to_send)
         self.assertTrue(
             self.services_monitoring.down_services == [message1]
+        )
+
+    def test_still_down_multiple_services(self):
+        """
+        Test if a service is still down with multiple services
+        """
+        message1 = Message('Service 1', 'Message 1', Message.ERROR)
+        message2 = Message('Service 2', 'Message 2', Message.ERROR)
+        notifications = [message1, message2]
+        self.services_monitoring.down_services = [message1]
+
+        notifications_to_send = \
+            self.services_monitoring.manage_notifications(notifications)
+
+        self.assertTrue(notifications_to_send == [message2])
+        self.assertTrue(
+            self.services_monitoring.down_services == [message1, message2]
         )
